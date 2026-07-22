bfa584059f61032722d66b6faf769f1a4cd5dd0b
BILAL12317 <mohamedbilal7708962545@gmail.com>
2026-07-22 17:42:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+# Bot Configuration
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Admin Telegram ID
+ADMIN_ID = 5053534694
+
+# Shop Details
+SHOP_NAME = "BILAL PANEL SHOP"
+SUPPORT_USERNAME = "@BILALPANEL3"
+
+# Payment
+UPI_ID = "mohamedbilal20116-1@okicici"
+
+# Prices
+PRICES = {
+    "1 Day": "₹60",
+    "3 Days": "₹130",
+    "7 Days": "₹220",
+    "15 Days": "₹460",
+    "30 Days": "₹700"
+}
