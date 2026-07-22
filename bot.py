b02d9086a2945334f26d8777db92ec84ba4509cb
BILAL12317 <mohamedbilal7708962545@gmail.com>
2026-07-22 19:40:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,12 @@
+import os
 import telebot
 
-BOT_TOKEN = 8844663921:AAE8xpgYFxXwS3XgNu6IfYXe4Y0XqSiohr0
+BOT_TOKEN = os.getenv("8844663921:AAE8xpgYFxXwS3XgNu6IfYXe4Y0XqSiohr0")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Hello 👋 Bilal Bot working!")
+    bot.reply_to(message, "Bilal bot working ✅")
 
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.reply_to(message, message.text)
-
-print("Bot is running...")
 bot.infinity_polling()
