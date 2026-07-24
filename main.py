7a639988a973ca187a85a657084b1f582b036323
BILAL12317 <mohamedbilal7708962545@gmail.com>
2026-07-24 16:52:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from telegram.ext import (
     CallbackQueryHandler,
     ContextTypes,
 )
-
+from telegram.ext import MessageHandler, filters
+from rembg import remove
+from io import BytesIO
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
