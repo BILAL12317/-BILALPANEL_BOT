272d60371b59ac07e8a48180d9e5affe7a85a404
BILAL12317 <mohamedbilal7708962545@gmail.com>
2026-07-27 22:09:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ def main():
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button))
-   app.add_handler(MessageHandler(filters.PHOTO, remove_bg))
+    app.add_handler(MessageHandler(filters.PHOTO, remove_bg))
 
-    print("✅ BILAL PANEL SHOP Bot Started")
     app.run_polling()
